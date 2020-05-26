75643612ff92fd33799ebda134c8df823c581ce9
Shawn Alexander <shawn.k.alexander@gmail.com>
2020-05-26 04:05:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -207,7 +207,7 @@ def player_numbers(team)
   
    hash.each do |status, data|
    data.each do |element, key|
-   binding.pry
+ 
    if data[:team_name] == team
      count = 0 
      while count < key.length do
